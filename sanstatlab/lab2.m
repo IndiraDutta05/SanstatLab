cd3ac16b31cddc32966b1b7165d2cb86a982c3c1
IndiraDutta05 <108519594+IndiraDutta05@users.noreply.github.com>
2025-05-07 11:28:33 +0200

    Update lab2.m

--- sanstatlab/lab2.m
+++ sanstatlab/lab2.m
@@ -37,3 +37,31 @@ axis([b1 b2 0 101])  % Tar bort outnyttjat utrymme i figuren
 % Ritar ut det sanna värdet
 plot([mu mu], [0 101], 'g')
 hold off
+
+%% Problem 2: Maximum likelihood/Minsta kvadrat
+M = 1e4;
+b = 4;
+x = raylrnd(b, M, 1);  % Generera M värden från Rayleigh-fördelning
+
+% Rita histogram med densitet
+hist_density(x, 40)
+hold on
+
+% ML- och MK-skattningar (byt ut med dina faktiska formler)
+my_est_ml = sqrt(mean(x.^2)/2);   % Exempel på ML-skattning för Rayleigh
+my_est_mk = sqrt(2/pi) * mean(x); % Exempel på MK-skattning
+
+% Rita ut skattningarna och det sanna värdet
+plot(my_est_ml, 0, 'r*', 'DisplayName', 'ML-skattning')
+plot(my_est_mk, 0, 'g*', 'DisplayName', 'MK-skattning')
+plot(b, 0, 'ro', 'DisplayName', 'Sant värde')
+legend
+hold off
+
+%% Problem 2: Maximum likelihood/Minsta kvadrat (forts.)
+% Rita upp täthetsfunktionen med ML-skattningen
+hold on
+x_vals = 0:0.1:6;
+plot(x_vals, raylpdf(x_vals, my_est_ml), 'r', 'DisplayName', 'Täthetsfunktion (ML)')
+legend
+hold off
